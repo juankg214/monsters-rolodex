c815f0d1fd64935e5ea78e609a265d503a1927f9
Juan Camilo Gomez Lopez <juankg214@gmail.com>
2020-03-31 23:37:09 -0500

    adding gh

---
+++ build/precache-manifest.a6de576d02856b3b5d554c561a7a6e08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad3c3c0bacc2ccbf4e29f203814517d4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "91f476d5f29a7a1cc382",
+    "url": "/monsters-rolodex/static/css/main.b095236c.chunk.css"
+  },
+  {
+    "revision": "a18d43196547948386f9",
+    "url": "/monsters-rolodex/static/js/2.8ebdf4f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.8ebdf4f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91f476d5f29a7a1cc382",
+    "url": "/monsters-rolodex/static/js/main.27c1ccad.chunk.js"
+  },
+  {
+    "revision": "345913ed4d11f5710073",
+    "url": "/monsters-rolodex/static/js/runtime-main.09132acd.js"
+  }
+]);
